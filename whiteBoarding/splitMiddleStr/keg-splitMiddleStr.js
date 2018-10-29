2fb222fc5574e952cf072dcdb5cc5db3a9d9f518
Kyle Gillett <kylegillett@gmail.com>
2018-10-28 22:35:34 -0600

    fixed requested changes

--- whiteBoarding/splitMiddleStr/keg-splitMiddleStr.js
+++ whiteBoarding/splitMiddleStr/keg-splitMiddleStr.js
@@ -1,13 +1,14 @@
 // Here's a start...10-28-18
 
-str1 = abcdefghijklmnopqrstuvxyz
-str2 = 123456789
+let str1 = 'abcdefghijklmnopqrstuvxyz'
+let str2 = '123456789'
 
-function(str1, str2){
+function splitMiddle(str1, str2) {
   let l2 = str2.length
   let l1 = str1.length
-  let a = str1.splice(0, x)
-  let b = str2.splice(x, y - 1)
+  let a = str1.slice(0, l2)
+  let b = str1.slice(l2, l1 - 1)
   let answer = b + a + str2
   return answer
 }
+console.log(splitMiddle(str1, str2))
